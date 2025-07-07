9a6e01261c4003bd751e12c891ad497f1484bd0d
edmeow <p.oksana.a@mail.ru>
2025-07-07 17:10:49 +0300

    pass ref through input and add styles export

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,37 +3,39 @@ import FormLabel from "@mui/material/FormLabel";
 import Typography from "@mui/material/Typography";
 import TextField, { type TextFieldProps } from "@mui/material/TextField";
 import { getVariantBySize } from "./utils";
+import React from "react";
 
 type InputProps = TextFieldProps & {
   startAdornment?: React.ReactNode;
   endAdornment?: React.ReactNode;
 };
 
-const Input = ({
-  label,
-  startAdornment,
-  endAdornment,
-  ...restProps
-}: InputProps) => {
-  return (
-    <Box>
-      <FormLabel disabled={restProps.disabled}>
-        <Typography component="div" variant={getVariantBySize(restProps.size)}>
-          {label}
-        </Typography>
-      </FormLabel>
-      <TextField
-        {...restProps}
-        slotProps={{
-          input: {
-            ...restProps.slotProps,
-            startAdornment,
-            endAdornment,
-          },
-        }}
-      />
-    </Box>
-  );
-};
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ label, startAdornment, endAdornment, ...restProps }, ref) => {
+    return (
+      <Box>
+        <FormLabel disabled={restProps.disabled}>
+          <Typography
+            component="div"
+            variant={getVariantBySize(restProps.size)}
+          >
+            {label}
+          </Typography>
+        </FormLabel>
+        <TextField
+          {...restProps}
+          inputRef={ref}
+          slotProps={{
+            input: {
+              ...restProps.slotProps,
+              startAdornment,
+              endAdornment,
+            },
+          }}
+        />
+      </Box>
+    );
+  }
+);
 
 export default Input;
